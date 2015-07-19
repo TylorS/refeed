4ecc8fe7c8803d46ca7fe10e687426784403a01a
Tylor Steinberger <tlsteinberger167@gmail.com>
2015-07-19 18:10:03 -0400

    Make setup.py actually work

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 
 setup(name="refeed",
+      packages=['refeed'],
       version="0.1.0",
       description="Feedly Python API Wrapper",
       author="Tylor Steinberger",
